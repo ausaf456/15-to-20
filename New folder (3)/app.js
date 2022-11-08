aaebf562cdb56d08f1d89d2343872b305f0c8acf
ausaf456 <ausafshah456@gmail.com>
2022-11-08 21:13:28 +0500

    15 to 20

---
+++ New folder (3)/app.js
@@ -0,0 +1,59 @@
+
+//chapter 15
+// var array = ["Mango", "Banana", "Apple", "Pineaaple", "Peach"];
+// console.log(array);
+
+
+//chapter 16
+// var array =["cat","dog","mouse","Elephant"];
+// console.log(array);
+
+// array.push("Tiger");
+// array.pop();
+
+//chapter 17
+//  var array =["Tomato","Potato","Anion","Carrot"];
+//  console.log(array);
+//  array.unshift("Cabbage","Peas");
+// array.shift();
+
+// array.splice(2,2,"Colly flower","Cucumber" );
+// array.slice(2,2);
+
+
+// Chapter 18
+// for (var i = 0; i < array.length; i++) {
+//     console.log(array[i]);
+// }
+
+// Chapter 19
+
+// var name = "Hamza"
+// for (var i = 0; i < 5; i++) {
+    
+// if(name=="Ausaf"){
+//     name="Hamza"
+    
+   
+
+// }else{
+//     name="Ausaf"
+// }
+// console.log(name);
+// }
+
+
+
+
+// Chapter 20
+// var  firstname=["Roman","Brock","Triple","John"]
+// var  lastname=["Reigns","Lesnar","H"]
+// var  fullname=[]
+
+// for (var i = 0; i < firstname.length; i++) {
+//     for (var j = 0; j < lastname.length; j++) {
+    
+//         fullname.push(firstname[i]+lastname[j]);
+//     }   
+//     console.log(fullname);
+// }
